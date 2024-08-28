dcfa73e46baa1cddff3914c9e4c24495689b24f6
Guilherme Ribeiro <guilhermeprog2002@gmail.com>
2024-08-28 16:58:58 -0300

    Criando um admin

--- gerenciar/produto/admin.py
+++ gerenciar/produto/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Produto
 # Register your models here.
+#esse produto vem lá da pasta produto models.py aquela classe 
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+# é o campo aonde aparece la na tela as colunas
+    list_display= (
+        '__str__',
+        'importado',
+        'ncm',
+        'preco',
+        'estoque',
+        'estoque_minimo',
+    )
+    search_fields = ('produto',) # campo de busca
+    list_filter = ('importado',) #  lista de filtrados na direita da tela
+    
--- gerenciar/produto/models.py
+++ gerenciar/produto/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Produto (models.Model):
     importado = models.BooleanField(default=False)
     ncm = models.CharField('NCM', max_length=8)
     produto = models.CharField(max_length= 100, unique=True)
+    preco = models.DecimalField('preço', max_digits = 7, decimal_places= 2, default = 0)
     estoque = models.IntegerField('estoque atual')
     estoque_minimo = models.PositiveBigIntegerField('estoque mínimo', default= 0)
     
