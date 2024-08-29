a19678b551364917f5ded57637beaaffe0d0aac8
Guilherme Ribeiro <guilhermeprog2002@gmail.com>
2024-08-28 22:32:31 -0300

    Criado o produto_list tela aonde fica os produtos

--- gerenciar/urls.py
+++ gerenciar/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('gerenciar.core.urls')),
+    path('produto/', include('gerenciar.produto.urls')),
     path('admin/', admin.site.urls),
 ]
