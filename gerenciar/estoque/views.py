7db32ef84c835d3cfb8072d3c055f3b2206f3126
Guilherme Ribeiro <guilhermeprog2002@gmail.com>
2024-09-17 16:50:30 -0300

    Colocando classe na minha view

--- gerenciar/estoque/views.py
+++ gerenciar/estoque/views.py
@@ -1,6 +1,7 @@
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
+from django.views.generic import ListView
 from gerenciar.produto.models import Produto
 from .models import Estoque, EstoqueEntrada, EstoqueSaida, EstoqueItens
 from .forms import EstoqueForm, EstoqueItensForm
@@ -15,6 +16,15 @@ def estoque_entrada_list(request):
             }
     return render(request, template_name, context)
 
+class EstoqueEntradaList(ListView):
+    model = EstoqueEntrada
+    template_name = 'estoque_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueEntradaList, self).get_context_data(**kwargs)
+        context['titulo' ] = 'Entrada'
+        context['url_add' ] = 'estoque:estoque_entrada_add'
+        return context
 
 # Create your views here.
 def estoque_entrada_detail(request, pk):
@@ -23,6 +33,7 @@ def estoque_entrada_detail(request, pk):
     context = {'object': obj,
                 'url_list': 'estoque:estoque_entrada_list'}
     return render(request, template_name, context)
+
 
 def dar_baixa_estoque(form):
     # Pega os produtos a partir da instância do formulário (Estoque).
@@ -52,6 +63,16 @@ def estoque_saida_list(request):
                 'url_add': 'estoque:estoque_saida_add'
             }
     return render(request, template_name, context)
+
+class EstoqueSaidaList(ListView):
+    model = EstoqueSaida
+    template_name = 'estoque_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstoqueSaidaList, self).get_context_data(**kwargs)
+        context['titulo' ] = 'Entrada'
+        context['url_add' ] = 'estoque:estoque_saida_add'
+        return context
 
 # Create your views here.
 def estoque_saida_detail(request, pk):
@@ -92,6 +113,7 @@ def estoque_add(request, template_name, movimento, url):
         formset = item_estoque_formset(instance=estoque_form, prefix='estoque')
     context = {'form': form, 'formset': formset }
     return context
+
 
 def estoque_saida_add(request):
     template_name = 'estoque_saida_form.html'
