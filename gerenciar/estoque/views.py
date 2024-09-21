dcfe563104edb7c8144c2ae51ee581eb91ee8ef6
Guilherme Ribeiro <guilhermeprog2002@gmail.com>
2024-09-21 16:16:01 -0300

    refatorando o Estoque datail

--- gerenciar/estoque/views.py
+++ gerenciar/estoque/views.py
@@ -35,14 +35,11 @@ def estoque_entrada_detail(request, pk):
                 'url_list': 'estoque:estoque_entrada_list'}
     return render(request, template_name, context)
 
-class EstoqueEntradaDetail(DetailView):
-    model = EstoqueEntrada
+
+class EstoqueDetail(DetailView):
+    model = Estoque
     template_name = 'estoque_detail.html'
-    
-    def get_context_data(self, **kwargs):
-        context=super(EstoqueEntradaDetail, self).get_context_data(**kwargs)
-        context['url_list'] = 'estoque:estoque_entrada_list'
-        return context
+
 
 def dar_baixa_estoque(form):
     # Pega os produtos a partir da instância do formulário (Estoque).
@@ -92,7 +89,7 @@ def estoque_saida_detail(request, pk):
                 'url_list': 'estoque:estoque_saida_list'
                 }
     return render(request, template_name, context)
-class EstoqueSaidaDetail(DetailView):
+
     model = EstoqueSaida
     template_name = 'estoque_detail.html'
     
